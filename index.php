9853157526ed2d8114ea2cc1f966a0733e7018f7
Bijbrengen <slagter@bijbrengen.nl>
2020-03-20 12:10:37 +0100

    Added view and connect to learning goals list

--- index.php
+++ index.php
@@ -67,13 +67,46 @@ class Menu_Dropdown_Learning_goals
         $this->learning_goals = $learning_goals;
     }
 
+    public function get_goal_description( string $title = "", string $name = "", string $description = "" )
+    {
+        $edit_link =
+            '<a class="goal_edit" href="' .
+                'https://www.leerdoeleditor.nl' .
+                '?name=' .  urlencode( $name ) .
+                '&description=' . urlencode( $description ) .
+                '" target="_blank">' .
+                '&nbsp;&#x2710;&nbsp;' .
+            '</a>';
+        $view_link =
+            '<a class="goal_view" href="' .
+                'https://api.hedude.com/goal/html/nl' .
+                '?title=' .  urlencode( $title ) .
+                '" target="_blank">' .
+                '&nbsp;&#x1F441;&nbsp;' .
+            '</a>';
+        $connect_link =
+            '<a class="goal_view" href="' .
+                'https://www.leerobject.org' .
+                '?type=goal' .
+                '&title=' .  urlencode( $title ) .
+                '" target="_blank">' .
+                '&nbsp;&#x26AF;&nbsp;' .
+            '</a>';
+        return $edit_link . $view_link . $connect_link . $name . '&nbsp;<span>' . $description . '</span>';
+    }
+
+    public function get_label_tag( string $title = "", string $name = "", string $description = "" )
+    {
+        return '<label class="goal_list_label" for="' . $title . '">' . $this->get_goal_description( $title, $name, $description ) . '</label>';
+    }
+
     public function get( string $indent = '    ' ) : string
     {
         $item = reset ( $this->learning_goals );
         $menu  = $indent . '<ul class="goal_list margin-top-lg margin-bottom-lg">' . PHP_EOL;
         $menu .= $indent . $this->gap . '<li class="goal_list_item goal_list_item--has-children">' . PHP_EOL;
         $menu .= $indent . $this->gap . $this->gap . '<input class="goal_list_input" type="checkbox" name ="' . $item[ 'title' ] . '" id="' . $item[ 'title' ] . '">' . PHP_EOL;
-        $menu .= $indent . $this->gap . '<label class="goal_list_label" for="' . $item[ 'title' ] . '"><a class="goal_edit" href="https://www.leerdoeleditor.nl?name=' .  urlencode( $item[ 'name' ] ) . '&description=' . urlencode( $item[ 'description' ] ) . '" target="_blank">&nbsp;&#x2710;&nbsp;</a>' . $item[ 'name' ] . '&nbsp;<span>' . $item[ 'description' ] . '</span></label>' . PHP_EOL;
+        $menu .= $indent . $this->gap . $this->get_label_tag( $item[ 'title' ], $item[ 'name' ], $item[ 'description' ] ) . PHP_EOL;
         $menu .= PHP_EOL;
         $menu .= $this->get_Submenu( $item['children'], $indent . $this->gap . $this->gap );
         $menu .= $indent . $this->gap . '</li>' . PHP_EOL;
@@ -99,14 +132,14 @@ class Menu_Dropdown_Learning_goals
             {
                 $view .= $indent . $this->gap . '<li class="goal_list_item goal_list_item--has-children">' . PHP_EOL;
                 $view .= $indent . $this->gap . $this->gap . '<input class="goal_list_input" type="checkbox" name ="' . $item[ 'title' ] . '" id="' . $item[ 'title' ] . '">' . PHP_EOL;
-                $view .= $indent . $this->gap . $this->gap . '<label class="goal_list_label" for="' . $item[ 'title' ] . '"><a class="goal_edit" href="https://www.leerdoeleditor.nl?name=' .  urlencode( $item[ 'name' ] ) . '&description=' . urlencode( $item[ 'description' ] ) . '" target="_blank">&nbsp;&#x2710;&nbsp;</a>' . $item[ 'name' ] . '&nbsp;<span>' . $item[ 'description' ] . '</span></label>' . PHP_EOL;
+                $view .= $indent . $this->gap . $this->gap . $this->get_label_tag( $item[ 'title' ], $item[ 'name' ], $item[ 'description' ] ) . PHP_EOL;
                 $view .= PHP_EOL;
                 $view .=  $this->get_Submenu( $item['children'],  $indent . $this->gap . $this->gap, $level + 1 );
                 $view .=  $indent . $this->gap . '</li>' . PHP_EOL;
             }
             else
             {
-                $view .=  $indent . $this->gap . '<li class="goal_list_item goal_list_label goal_list_label--icon-img"><a class="goal_edit" href="https://www.leerdoeleditor.nl?name=' .  urlencode( $item[ 'name' ] ) . '&description=' . urlencode( $item[ 'description' ] ) . '" target="_blank">&nbsp;&#x2710;&nbsp;</a>' . $item[ 'name' ] . '&nbsp;<span>' . $item[ 'description' ] . '</span></li>' . PHP_EOL;
+                $view .=  $indent . $this->gap . '<li class="goal_list_item goal_list_label goal_list_label--icon-img">' . $this->get_goal_description( $item[ 'title' ], $item[ 'name' ], $item[ 'description' ] ) . '</li>' . PHP_EOL;
             }
         }
         $view .= $indent . '</ul>' . PHP_EOL;
